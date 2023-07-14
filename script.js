055b586ceaa5c5fe5f797df3f58f55d4531b2e98
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-07-14 22:43:57 +0530

    upated3 js

--- script.js
+++ script.js
@@ -27,3 +27,21 @@ setInterval(function(){
     }
 }, 10);
 
+ function jump(){
+     jummping = 1;
+     let jumpCount = 0;
+     var jumpInterval = setInterval(function(){
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if((characterTop>6)&& (jumpCount<15)){
+            character.style.top = (characterTop-5)+ "px";
+        }
+        if(jumpCount > 20){
+            clearInterval(jumpInterval);
+            jummping = 0;
+            jumpCount =0;
+     }
+     jumpCount++;
+     }, 10);
+
+ }
+

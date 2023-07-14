fd2f61a89b543e863f48c0883481bd4ac65a751f
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-07-14 22:31:14 +0530

    updated2 js

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ hole.addEventListener('animationiteration', () =>{
     counter++;
 });
 
+setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if(jummping == 0){
+        character.style.top = (characterTop+3)+"px";
+    }
+
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("top"));
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    var cTop = -(500-characterTop);
+    if((characterTop > 400) || ((blockLeft < 20)&& (blockLeft >- 50) && ((cTop<holeTop) || (cTop>holeTop+130)))){
+        alert("Game Over. Score: "+(counter-1));
+        character.style.top = 100 + "px";
+        counter = 0;
+    }
+}, 10);
+
